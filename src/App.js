30cf310ed671b84a00735c65804aceb4c1fa38b5
leoarthurwall <leowall93@gmail.com>
2022-09-17 20:20:31 +0100

    added an if else statement with an alert to prompt when the quiz has finished

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import questions from "./questions";
 import { useState } from "react";
 
 function App() {
-  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
   const handleQuestionClick = () => {
-    const nextQuestion = currentQuestion + 1
-    setCurrentQuestion(nextQuestion)
-  }
+    const nextQuestion = currentQuestion + 1;
+
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      alert("you have finished the quiz");
+    }
+  };
   return (
     <div className="App">
       <div className="quiz-container">
         <div className="left-container">
-          <h1 className="question-number">Question {currentQuestion + 1}<span className="total-questions">/{questions.length}</span></h1>
-          <h3 className="question">{questions[currentQuestion].questionText}</h3>
+          <h1 className="question-number">
+            Question {currentQuestion + 1}
+            <span className="total-questions">/{questions.length}</span>
+          </h1>
+          <h3 className="question">
+            {questions[currentQuestion].questionText}
+          </h3>
         </div>
         <div className="right-container">
-        {questions[currentQuestion].answerOptions.map((answerOption) => <button className="answer" onClick={handleQuestionClick}>{answerOption.answerText}</button>)}
+          {questions[currentQuestion].answerOptions.map((answerOption) => (
+            <button className="answer" onClick={handleQuestionClick}>
+              {answerOption.answerText}
+            </button>
+          ))}
         </div>
       </div>
     </div>
