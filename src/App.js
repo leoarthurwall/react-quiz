603dc63da7c4c954bce4cf1cf8e05e6d61564366
leoarthurwall <leowall93@gmail.com>
2022-09-17 20:35:47 +0100

    added a ternary operator to contitionaly display the score card once the quiz has finished. at the moment it is not functioning dynamically, but the jsx and scc is styled

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [finalScore, setFinalScore] = useState(true);
 
   const handleQuestionClick = () => {
     const nextQuestion = currentQuestion + 1;
@@ -17,22 +18,30 @@ function App() {
   return (
     <div className="App">
       <div className="quiz-container">
-        <div className="left-container">
-          <h1 className="question-number">
-            Question {currentQuestion + 1}
-            <span className="total-questions">/{questions.length}</span>
-          </h1>
-          <h3 className="question">
-            {questions[currentQuestion].questionText}
-          </h3>
-        </div>
-        <div className="right-container">
-          {questions[currentQuestion].answerOptions.map((answerOption) => (
-            <button className="answer" onClick={handleQuestionClick}>
-              {answerOption.answerText}
-            </button>
-          ))}
-        </div>
+        {finalScore ? (
+          <div className="final-score-container">
+            <h2>You scored 1 out of {questions.length}</h2>
+          </div>
+        ) : (
+          <>
+            <div className="left-container">
+              <h1 className="question-number">
+                Question {currentQuestion + 1}
+                <span className="total-questions">/{questions.length}</span>
+              </h1>
+              <h3 className="question">
+                {questions[currentQuestion].questionText}
+              </h3>
+            </div>
+            <div className="right-container">
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <button className="answer" onClick={handleQuestionClick}>
+                  {answerOption.answerText}
+                </button>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
